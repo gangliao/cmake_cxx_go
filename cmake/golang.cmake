004de4fda8da7116bb3a7a24fc6a445abd5ef539
liaogang <liaogang@baidu.com>
2017-05-04 08:00:45 +0800

    git add go lib

--- cmake/golang.cmake
+++ cmake/golang.cmake
@@ -7,17 +7,37 @@ endfunction(ExternalGoProject_Add)
 
 function(ADD_GO_EXECUTABLE NAME MAIN_SRC)
   get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
-  add_custom_target(${NAME})
-  add_custom_command(TARGET ${NAME}
+  add_custom_command(OUTPUT ${OUTPUT_DIR}/.timestamp
                     COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build 
                     -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
                     ${CMAKE_GO_FLAGS} ${MAIN_SRC}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                     DEPENDS ${MAIN_SRC_ABS})
-  foreach(DEP ${ARGN})
-    add_dependencies(${NAME} ${DEP})
-  endforeach()
 
-  add_custom_target(${NAME}_go ALL DEPENDS ${NAME})
+  add_custom_target(${NAME} ALL DEPENDS ${OUTPUT_DIR}/.timestamp ${ARGN})
   install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
 endfunction(ADD_GO_EXECUTABLE)
+
+function(ADD_GO_LIBRARY NAME BUILD_TYPE MAIN_SRC)
+  if(BUILD_TYPE STREQUAL "STATIC")
+    set(BUILD_MODE -buildmode=c-archive)
+    set(LIB_NAME "lib${NAME}.a")
+  else()
+    set(BUILD_MODE -buildmode=c-shared)
+    if(APPLE)
+      set(LIB_NAME "lib${NAME}.dylib")
+    else()
+      set(LIB_NAME "lib${NAME}.so")
+    endif()
+  endif()
+
+  get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
+  add_custom_command(OUTPUT ${OUTPUT_DIR}/.timestamp
+                    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+                    -o "${CMAKE_CURRENT_BINARY_DIR}/${LIB_NAME}"
+                    ${CMAKE_GO_FLAGS} ${MAIN_SRC}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    DEPENDS ${MAIN_SRC_ABS})
+  
+  add_custom_target(${NAME} ALL DEPENDS ${OUTPUT_DIR}/.timestamp ${ARGN})
+endfunction(ADD_GO_LIBRARY)
