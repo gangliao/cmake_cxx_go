bd280d1b59896b77420e2be7c9471308b9df6ec4
liaogang <liaogang@baidu.com>
2017-05-04 19:08:37 +0800

    update cuda cmake

--- src/cuda/test_add.h
+++ src/cuda/test_add.h
@@ -1,1 +1,18 @@
+#ifndef TEST_ADD_H
+#define TEST_ADD_H
+
+// __cplusplus gets defined when a C++ compiler processes the file
+#ifdef __cplusplus
+// extern "C" is needed so the C++ compiler exports the symbols without name
+// manging.
+extern "C" {
+#endif
+
 int test_add(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
