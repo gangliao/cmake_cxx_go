9e73e047ddee2cc07f16f79603884d178b1f7cd1
Yi Wang <yiwang01@baidu.com>
2017-05-03 17:00:38 -0700

    Add Go project recordio and cmake function add_go_library

--- cmake/golang.cmake
+++ cmake/golang.cmake
@@ -5,19 +5,36 @@ function(ExternalGoProject_Add TARG)
   add_custom_target(${TARG} env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} get ${ARGN})
 endfunction(ExternalGoProject_Add)
 
-function(ADD_GO_EXECUTABLE NAME MAIN_SRC)
-  get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
+function(add_go_executable NAME)
+  get_filename_component(SRC_ABS ${CMAKE_CURRENT_SOURCE_DIR} ABSOLUTE)
   add_custom_target(${NAME})
   add_custom_command(TARGET ${NAME}
-                    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build 
+                    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
                     -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
-                    ${CMAKE_GO_FLAGS} ${MAIN_SRC}
+                    ${CMAKE_GO_FLAGS}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                    DEPENDS ${MAIN_SRC_ABS})
+                    DEPENDS file(GLOB ${SRC_ABS} "*.go"))
   foreach(DEP ${ARGN})
     add_dependencies(${NAME} ${DEP})
   endforeach()
 
   add_custom_target(${NAME}_go ALL DEPENDS ${NAME})
   install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
-endfunction(ADD_GO_EXECUTABLE)
+endfunction(add_go_executable)
+
+function(add_go_library NAME)
+  get_filename_component(SRC_ABS ${CMAKE_CURRENT_SOURCE_DIR} ABSOLUTE)
+  add_custom_target(${NAME})
+  add_custom_command(TARGET ${NAME}
+                    COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
+                    -o "${CMAKE_CURRENT_BINARY_DIR}/lib${NAME}.a"
+                    ${CMAKE_GO_FLAGS}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    DEPENDS file(GLOB ${SRC_ABS} "*.go"))
+  foreach(DEP ${ARGN})
+    add_dependencies(${NAME} ${DEP})
+  endforeach()
+
+  add_custom_target(${NAME}_go ALL DEPENDS ${NAME})
+  install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
+endfunction(add_go_library)
